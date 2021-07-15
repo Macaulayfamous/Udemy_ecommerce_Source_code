983ebe8f9c5ec5b81ea2cf03e58abc24120e1f09
macaulayfamous <pythonfamous@gmail.com>
2021-07-15 06:08:44 +0100

    Added Django-heroku

--- course/settings.py
+++ course/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
