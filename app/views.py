d0ff84fd3fea67b53079ec197cae2d314a968ba8
macaulayfamous <pythonfamous@gmail.com>
2021-07-20 16:57:15 +0100

    added

--- app/views.py
+++ app/views.py
@@ -7,17 +7,16 @@ from orders.models import OrderProduct
 # Create your views here.
 
 def home(request):
-   
     products = Product.objects.all().filter(is_available=True)
     for product in products:
         
          reviews  = ReviewRating.objects.filter(product_id=product.id, status=True)
           
 
-         context = {
-            'products' : products,
-            'reviews': reviews,
-        }
+    context = {
+        'products' : products,
+        'reviews': reviews,
+    }
     return render(request, 'app/home.html',context)
 
 
