a143d4bc60f504c1ff31f4116057fc2a554b1b6a
macaulayfamous <pythonfamous@gmail.com>
2021-07-20 16:40:12 +0100

    added

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ def home(request):
     products = Product.objects.all().filter(is_available=True)
     for product in products:
         
-         reviews  = ReviewRating.objects.filter(product_id=product.id, status=True)
+        reviews  = ReviewRating.objects.filter(product_id=product.id, status=True)
           
 
     context = {
